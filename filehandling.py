0e4e8a7c8052c0b5fe3588c6b566c507fb8f0609
anjana321-anj <thisisanjanag@gmail.com>
2026-02-07 22:56:34 +0545

    Update filehandling.py

--- filehandling.py
+++ filehandling.py
@@ -37,7 +37,6 @@ with open('employees.txt', 'r') as source_file, open('management.txt', 'w') as d
     for line in source_file:
         if 'Python' in line:
             dest_file.write(line)
-return "Filtering complete. Lines containing 'Python' have been copied to management.txt."
 
 #QN.NO-6
 with open('numbers.txt', 'r') as source, open('squared.txt', 'w') as destination:
@@ -46,18 +45,18 @@ with open('numbers.txt', 'r') as source, open('squared.txt', 'w') as destination
             number = int(line.strip())
             squared_number = number ** 2
             destination.write(str(squared_number) + '\n')
-return "Calculation complete. Results saved to squared.txt."
+
 
 #QN.NO-7
 user_message = input("Enter the message you want to log: ")
 with open('history.log', 'a') as file:
     file.write(user_message + '\n')
-return "Message successfully added to history.log."
+
 
 #QN.NO-8
 with open('input.txt', 'r') as infile, open('output.txt', 'w') as outfile:
     for line in infile:
         capitalized_line = line.upper()
         outfile.write(capitalized_line)
-return "Conversion complete. All text in output.txt is now capitalized."
+
 
